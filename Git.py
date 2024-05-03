5db2691f5d991c4a599c9f5abf460757270870a8
Nurik4409 <Nurmuhammad2220@icloud.com>
2024-05-02 21:55:53 -0700

    second changes

--- Git.py
+++ Git.py
@@ -1,3 +1,14 @@
-# git init чтобы создавать репозиторию
-# git status видеть визуально
-# git add . добавляет
+#1. git init чтобы создавать репозиторию
+#2. git status видеть визуально
+#3. git add . добавляет все изменения в git репозитория а точка означает
+#4. git commit -m 'example commit'
+#5. git push origin master
+
+
+
+
+
+
+
+
+
